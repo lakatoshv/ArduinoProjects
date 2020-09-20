bc7575230288d6be8e24da74b01b3085456a0572
Віталій Лакатош <some.pochta1@gmail.com>
2020-09-20 23:25:28 +0300

    Code refactoring

--- motionSensorWithLed.ino
+++ motionSensorWithLed.ino
@@ -8,6 +8,7 @@
 const int LED_PIN = 7;
 const int MOTION_SENSOR_PIN = 9;
 int motionSensorValue = 0;
+
 void setup()
 {
   pinMode(LED_PIN, OUTPUT);
@@ -19,7 +20,7 @@ void loop()
   motionSensorValue = digitalRead(MOTION_SENSOR_PIN);
   digitalWrite(LED_PIN, motionSensorValue);
 
-    if (val == 1) 
+    if (motionSensorValue == 1) 
     {
         digitalWrite(LED_PIN, LOW);
     }
--- smartLamp.ino
+++ smartLamp.ino
@@ -1,20 +1,29 @@
-//за допомогою фоторезистора + резистора + світлодіода зробити розумну лампу. 
-// Тобто коли темно в аудиторії включається лампа автоматично
+/// <summary>
+/// Smart lamp.
+/// When it is dark in the room, the lamp turns on automatically
+/// </summary>
+// <copyright file="smartLamp.ini" company="lakatoshvv">
+// Copyright (c) lakatoshvv. All rights reserved.
+// </copyright>
+
+const int LED_PIN = 9;
+const int PHOTORESISTOR_PIN = 0;
+
 void setup()
 {
-  pinMode(9, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-  if (analogRead(0) > 0)
+  if (analogRead(PHOTORESISTOR_PIN) > 0)
   {
-    digitalWrite(9, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     delay(1000);
   }
   else
   {
-    digitalWrite(9, LOW);
+    digitalWrite(LED_PIN, LOW);
     delay(1000);
   }
 }

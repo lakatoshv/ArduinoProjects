9236bbce8d0e858fcffc79544c29dc547cd99c9b
Віталій Лакатош <some.pochta1@gmail.com>
2020-09-24 23:24:44 +0300

    Display different lights on led rgb

--- ledRGBv2.ino
+++ ledRGBv2.ino
@@ -10,11 +10,39 @@ void setup()
  
 void loop()
 {
-    digitalWrite(2,HIGH);
-    for(int x=5;x<13;x++)
-    {
-        digitalWrite(x,LOW);
-        delay(500);
-        digitalWrite(x,HIGH);
-    }
+   blue();
+   cycle(); 
+   green();
+   cycle();
+   red();
+   cycle();
+}
+ 
+void blue()
+{
+  digitalWrite(2,LOW);
+}
+ 
+void green()
+{
+  digitalWrite(3,LOW);
+}
+ 
+void red()
+{
+  digitalWrite(4,LOW);
+}
+ 
+ 
+void cycle()
+{
+  for(int x=5;x<13;x++)
+  {
+    digitalWrite(x,LOW);
+    delay(500);
+    digitalWrite(x,HIGH);
+  }
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
 }

08214573e77616a5f33d7ad1911d10b50d761bfd
Віталій Лакатош <some.pochta1@gmail.com>
2020-09-24 23:26:46 +0300

    Display random color on RGB Led

--- ledRGBv2.ino
+++ ledRGBv2.ino
@@ -1,5 +1,8 @@
+#define LED_PIN     6
+
 void setup()
 {
+  pinMode(A0,INPUT);
   //all pins are outputs and we are using 2 to 13
   for (int x =2;x<13;x++)
   {
@@ -10,36 +13,36 @@ void setup()
  
 void loop()
 {
-   blue();
-   cycle(); 
-   green();
-   cycle();
-   red();
    cycle();
 }
  
-void blue()
+void color()
 {
+  int random1 = (rand() % 4) + 1;
+  int random2 = (rand() % 4) + 1;
+  int random3 = (rand() % 4) + 1;
+
   digitalWrite(2,LOW);
-}
- 
-void green()
-{
   digitalWrite(3,LOW);
-}
- 
-void red()
-{
   digitalWrite(4,LOW);
+  if(2 == random1 || 2 == random2 || 2 == random3) {
+    digitalWrite(2,HIGH);
+  }
+  if(3 == random1 || 3 == random2 || 3 == random3) {
+    digitalWrite(3,HIGH);
+  }
+  if(4 == random1 || 4 == random2 || 4 == random3) {
+    digitalWrite(4,HIGH);
+  }
 }
- 
  
 void cycle()
 {
   for(int x=5;x<13;x++)
   {
+    color();
     digitalWrite(x,LOW);
-    delay(500);
+    delay(100);
     digitalWrite(x,HIGH);
   }
   digitalWrite(2,HIGH);

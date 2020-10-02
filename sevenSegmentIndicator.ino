b8199fbeb36757e8c88ead290ca82672afb0e442
Віталій Лакатош <some.pochta1@gmail.com>
2020-10-03 00:12:48 +0300

    Use seven segment indicator with joystic

--- sevenSegmentIndicator.ino
+++ sevenSegmentIndicator.ino
@@ -1,6 +1,11 @@
 #include "SevSeg.h"
 SevSeg sevseg; 
 
+int number = 0;
+
+#define joyX A0
+#define joyY A1
+
 void setup(){
     byte numDigits = 1;
     byte digitPins[] = {};
@@ -13,6 +18,27 @@ void setup(){
 }
 
 void loop(){
-    sevseg.setNumber(4);
-    sevseg.refreshDisplay();        
+    displayDigit(4);       
+}
+
+void useJoystic() {
+    // put your main code here, to run repeatedly:
+    int xValue = analogRead(joyX);
+    int yValue = analogRead(joyY);
+
+    //print the values with to plot or view    
+    if((xValue == 0 || yValue == 0) && number > 0){
+        number--;
+        displayDigit(number);
+    }
+    if((xValue == 1023 || yValue == 1023) && number < 9){
+        number++;   
+        displayDigit(number);
+    }
+}
+
+void displayDigit(int number) {
+    sevseg.setNumber(number);
+    delay(1000);
+    sevseg.refreshDisplay();
 }

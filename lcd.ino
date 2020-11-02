1d3474736137660577f374134d092e9bde7fcafd
Віталій Лакатош <some.pochta1@gmail.com>
2020-11-02 08:22:28 +0200

    Use lcd

---
+++ lcd.ino
@@ -0,0 +1,22 @@
+/// <summary>
+/// Use LCD.
+/// </summary>
+// <copyright file="lcd.ini" company="lakatoshvv">
+// Copyright (c) lakatoshvv. All rights reserved.
+// </copyright>
+
+// Liquid crystal display libraries
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  lcd.clear();
+  lcd.print("Hello World!");
+}

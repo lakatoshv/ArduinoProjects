626f524ffeb928597f8508133f2a766d81cf96f2
Віталій Лакатош <some.pochta1@gmail.com>
2020-12-07 16:18:28 +0200

    Use rgb to display rainbow. Display all red colors

---
+++ rgbRainbow.ino
@@ -0,0 +1,83 @@
+/// <summary>
+/// Use rgb to make rainbow.
+/// </summary>
+// <copyright file="rgbRainbow.ini" company="lakatoshvv">
+// Copyright (c) lakatoshvv. All rights reserved.
+// </copyright>
+
+const int POTENTIOMETER_PIN = A0;
+
+// RGB
+const int RED_PIN = 11;
+const int GREEN_PIN = 10;
+const int BLUE_PIN = 9;
+
+int red;
+int green;
+int blue;
+
+// pauses
+const int MIN_PAUSE=10;
+const int MAX_PAUSE=100;
+const int VIEW_PAUSE=2000;
+
+int pause;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    displayRainbowColors();
+}
+
+/// <summary>
+/// Display rainbow colors.
+/// </summary>
+void displayRainbowColors()
+{
+    fromRedToYellowColor();
+}
+
+/// <summary>
+/// From red to yellow colors.
+/// </summary>
+void fromRedToYellowColor()
+{
+    Serial.println("red - yellow");
+    red = 255;
+    green = 0;
+    blue = 0;
+    for(green = 0; green <= 255; green++) 
+    {
+        setRGB(red, green, blue); 
+    }
+    setpause();
+}
+
+/// <summary>
+/// Set color to RGB.
+/// </summary>
+/// <param name="r">red.</param>
+/// <param name="g">green.</param>
+/// <param name="b">blue.</param>
+void setRGB(int r, int g, int b)
+{
+    analogWrite(RED_PIN, r);
+    analogWrite(GREEN_PIN, g);
+    analogWrite(BLUE_PIN, b);
+    delay(pause);
+}
+
+/// <summary>
+/// Set pause time from potentiometer.
+/// </summary>
+void setpause()
+{
+    pause = map(analogRead(POTENTIOMETER_PIN), 0, 1024, MIN_PAUSE, MAX_PAUSE);
+    Serial.print("pause = ");
+    Serial.println(pause);
+    delay(VIEW_PAUSE); 
+}

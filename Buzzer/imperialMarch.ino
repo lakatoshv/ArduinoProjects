3cb3700cd067b26074310a2256347ba361b39de7
Vitalii Lakatosh <some.pochta1@gmail.com>
2021-12-28 23:32:23 +0200

    Add imperial march notes array

---
+++ Buzzer/imperialMarch.ino
@@ -0,0 +1,18 @@
+const int BUZZER_PIN = 11;
+
+//Imperial march notes array
+char melody[] = {
+  'G', 'G', 'G', 'E', 'H',
+  'G', 'E', 'H', 'G', '*',
+  'd', 'd', 'd', 'e', 'H',
+  'T', 'E', 'H', 'F',
+  'g', 'G', 'G', 'g', 't', 'e',
+  's', 's', 's', '*', 'U', 'r', 'c', 'B',
+  'H', 'A', 'H', '*', 'E', 'T', 'E', 'F',
+  'H', 'G', 'H', 'd',
+  'g', 'G', 'G', 'g', 't', 'f',
+  's', 's', 's', '*', 'U', 'r', 'c', 'B',
+  'H', 'A', 'H', '*', 'E', 'T', 'E', 'H',
+  'G', 'E', 'H', 'G',
+  '%', '%'
+};

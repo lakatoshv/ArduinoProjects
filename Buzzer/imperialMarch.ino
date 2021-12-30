2fe5f247deb861bbc46ae9502407ff021646ace5
Vitalii Lakatosh <some.pochta1@gmail.com>
2021-12-30 23:36:38 +0200

    Add durations of the notes

--- Buzzer/imperialMarch.ino
+++ Buzzer/imperialMarch.ino
@@ -141,3 +141,11 @@ int bb[] = {
 #define Db8 4434.92
 #define D8  4698.64
 #define Eb8 4978.03
+
+// DURATION OF THE NOTES
+#define BPM 120    //  you can change this value changing all the others
+#define H 2*Q //half 2/4
+#define Q 60000/BPM //quarter 1/4
+#define E Q/2   //eighth 1/8
+#define S Q/4 // sixteenth 1/16
+#define W 4*Q // whole 4/4
